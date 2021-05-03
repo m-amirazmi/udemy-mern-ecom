d6b0d2b62001519af0ada56039daf81058b5f94b
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-05-03 22:22:38 +0800

    express validator for sign up checker

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,11 +8,9 @@ exports.signup = (req, res) => {
     const user = new User(body);
     user.save((err, user) => {
         if (err) return res.status(400).json({ err: errorHandler(err) });
-
+        
         user.salt = undefined;
         user.hashed_password = undefined;
-
         res.json({ user });
-
     })
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const { signup } = require('../controllers/user');
+const { userSignupValidator } = require('../validator');
 
-router.post('/signup', signup);
+router.post('/signup', userSignupValidator, signup);
 
 module.exports = router;
