a1740116b6c338a0fe59fdb64f9aabc49aaa83fa
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-05-03 22:42:41 +0800

    signout

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -32,3 +32,8 @@ exports.signin = (req, res) => {
         return res.json({ token, user: { _id, name, email, role } })
     })
 }
+
+exports.signout = (req, res) => {
+    res.clearCookie('t');
+    res.json({ message: 'Successfully Signout' });
+}
