c5bf48fe2ee21c6ae1adb8439674750f3a3d3438
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-05-03 22:06:52 +0800

    friendly error message for duplicate signup email

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,11 +1,18 @@
+const { errorHandler } = require('../helpers/dbErrorHandler');
 const User = require('../models/user');
+
 
 exports.signup = (req, res) => {
     const { body } = req
 
     const user = new User(body);
     user.save((err, user) => {
-        if (err) return res.status(400).json({ err });
+        if (err) return res.status(400).json({ err: errorHandler(err) });
+
+        user.salt = undefined;
+        user.hashed_password = undefined;
+
         res.json({ user });
+
     })
 }
