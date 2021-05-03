b0320e0c0c8b2a68d93923c082be99a03d17e07b
Muhamad Amir <60350614+m-amirazmi@users.noreply.github.com>
2021-05-04 00:10:15 +0800

    isauth and isadmin

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
-const { requireSignin } = require('../controllers/auth');
+const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
 
-router.get('/secret/:userId', requireSignin, (req, res) => {
+router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (req, res) => {
     res.json({ user: req.profile })
 })
 
